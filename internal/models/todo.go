51ec30e99d7aee20f785cb74b01a5dc70ed47a0c
Hossam Eltohame <ha954074@gmail.com>
2026-03-06 16:28:29 +0200

    Add Todo model definition with JSON serialization

---
+++ internal/models/todo.go
@@ -0,0 +1,12 @@
+package models
+
+type Todo struct {
+	ID          int    `json:"id"`
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Completed   bool   `json:"completed"`
+	CreatedAt   string `json:"created_at"`
+	UpdatedAt   string `json:"updated_at"`
+	DeletedAt   string `json:"deleted_at,omitempty"`
+	IsDeleted   bool   `json:"is_deleted"`
+}
